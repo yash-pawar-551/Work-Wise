686d399055b6e77b292e0cee57999c424efd47cb
yash-pawar-551 <pawaryash379@gmail.com>
2025-02-14 17:04:31 +0530

    used hooks to manage data.

--- work-wise/src/components/JobListing.jsx
+++ work-wise/src/components/JobListing.jsx
@@ -1,6 +1,16 @@
 import PropTypes from "prop-types"
+import { useState } from "react"
 
 const JobListing = ({job}) => {
+
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if(!showFullDescription){
+    description = description.substring(0, 90) + '...';
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
                 <div className="p-4">
@@ -10,9 +20,9 @@ const JobListing = ({job}) => {
                   </div>
   
                   <div className="mb-5">
-                    {job.description}
+                    {description}
                   </div>
-  
+                  <button onClick={() => setShowFullDescription((p) => !p)} className="text-indigo-600">{showFullDescription ? 'Less' : 'More'}</button>
                   <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
   
                   <div className="border border-gray-100 mb-5"></div>
@@ -30,7 +40,7 @@ const JobListing = ({job}) => {
                     </a>
                   </div>
                 </div>
-              </div>
+      </div>
   )
 }
 
